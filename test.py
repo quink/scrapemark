f3659ffe498e58c89abbdd0c371c6359b9b8b18c
quink <quinks@gmail.com>
2011-08-06 17:48:02 +1000

    Coverage 70%

--- test.py
+++ test.py
@@ -14,6 +14,14 @@ class TestScrape(unittest.TestCase):
         data = ['{{ foo|html }}', '<a>hello</a>', {'foo':'<a>hello</a>'}]
         self.assertScrape(*data)
 
+    def test_multi(self):
+        data = ['{*<a>{{ [foo] }}</a>*}', '<em><a>hello</a><a>hello2</a></em>', {'foo':['hello','hello2']}]
+        self.assertScrape(*data)
+    
+    def test_standalone(self):
+        data = ['<a value="" value2="{{ foo|float }}" />', '<a value="yay" value2="3.0" />', {'foo': 3.0}]
+        self.assertScrape(*data)
+
     def adda(self, string):
         return string + "a"
     
